b09a85452807d466706655c9caaf995240a0c9ca
Gaku Yasui <yasui.gaku@a-tm.co.jp>
2019-02-10 23:41:17 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Hello World!'
+    }
+  },
+  head () {
+    return {
+      title: this.title,
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        { hid: 'description', name: 'description', content: 'My custom description' }
+      ]
+    }
+  }
+}
+</script>
